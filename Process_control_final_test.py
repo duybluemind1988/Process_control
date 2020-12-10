0ae0846eb641d464a7e525b4bdb828f3d185977c
duybluemind1988 <bluemind1988@gmail.com>
2020-12-10 17:21:44 +0700

    clear all

--- Process_control_final_test.py
+++ Process_control_final_test.py
@@ -1,7 +1,4 @@
-# add something from vs code dnn
-# add something 2
-# add something 3
-# update new branch
+#Clear all
 import streamlit as st
 import seaborn as sns; sns.set()
 import pandas as pd
